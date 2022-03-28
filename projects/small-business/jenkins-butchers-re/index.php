0536b26edee594d5e23c3b0654df29d67d7f968b
Caspar Leung <casparleung0219@gmail.com>
2022-03-28 18:26:36 +0100

    php include for index.php

--- projects/small-business/jenkins-butchers-re/index.php
+++ projects/small-business/jenkins-butchers-re/index.php
@@ -28,32 +28,8 @@
 	</svg>
 
 	<!-- || header section || -->
-	<header>
-		<!-- top logo -->
-		<a href="index.html">
-			<picture class="shop-logo">
-				<source type="image/svg+xml" srcset="imgs/logo-blue.svg">
-				<img src="imgs/logo-blue.png" alt="Jenkin's Butcher logo">
-			</picture>
-		</a>
-		<!-- top navigation bar -->
-		<nav id="top-nav">
-			<ul>
-				<li><a href="about.html">About</a></li>
-				<li><a href="special.html">Special</a></li>
-				<li><a href="products.html">Products</a></li>
-				<li><a href="contact.html">Contact</a></li>
-			</ul>
-		</nav>
-
-		<!-- basic contact information without link -->
-		<span class="upper">Mon-Fri 0800-1800 | Sat-Sun 1030-1700</span>
-		<div class="strips-upper"></div>	<!-- visual element - blue stripy awning -->
-		<address>
-			<span class="lower">41 Main Street, Ballyclare</span>
-		</address>
-		<div class="strips-lower"></div>	<!-- visual element - blue stripy awning -->
-	</header>
+	<!-- section included with php -->
+	<? include $_SERVER['DOCUMENT_ROOT'] . '/projects/small-business/jenkins-butchers-re/php-include/header.inc.php'; ?>
 
 	<!-- || main content section || -->
 	<main>
@@ -152,19 +128,8 @@
 	</main>
 
 	<!-- || footer section || -->
-	<footer>
-		<nav id="bottom-nav">	<!-- bottom navigation bar -->
-			<ul>
-				<li><a href="about.html">About</a></li>
-				<li><a href="special.html">Special</a></li>
-				<li><a href="products.html">Products</a></li>
-				<li><a href="contact.html">Contact</a></li>
-			</ul>
-		</nav>
-		<!-- website information -->
-		<p>Coursework by Caspar Leung - MA Web Design and Content Planning</p>
-		<p>University of Greenwich - 2022</p>
-	</footer>
+	<!-- section included with php -->
+	<? include $_SERVER['DOCUMENT_ROOT'] . '/projects/small-business/jenkins-butchers-re/php-include/footer.inc.php'; ?>
 
 </body>
 </html>
---
+++ projects/small-business/jenkins-butchers-re/php-include/footer.inc.php
@@ -0,0 +1,14 @@
+<!-- || footer section || -->
+<footer>
+	<nav id="bottom-nav">	<!-- bottom navigation bar -->
+		<ul>
+			<li><a href="about.html">About</a></li>
+			<li><a href="special.html">Special</a></li>
+			<li><a href="products.html">Products</a></li>
+			<li><a href="contact.html">Contact</a></li>
+		</ul>
+	</nav>
+	<!-- website information -->
+	<p>Coursework by Caspar Leung - MA Web Design and Content Planning</p>
+	<p>University of Greenwich - 2022</p>
+</footer>
