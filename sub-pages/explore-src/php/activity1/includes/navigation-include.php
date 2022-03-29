aa69c59858e3dc0fec1cfb6192f785b378f48eb1
Caspar Leung <casparleung0219@gmail.com>
2022-03-30 00:59:14 +0100

    adding activity 1 for php explore

---
+++ sub-pages/explore-src/php/activity1/includes/navigation-include.php
@@ -0,0 +1,7 @@
+<nav>
+	<ul>
+		<li><a href="index.php">Introduction</a></li>
+		<li><a href="craft-and-beauty.php">Craft and Beauty</a></li>
+		<li><a href="improvisation-and-limitations.php">Improvisation and Limitations</a></li>
+	</ul>
+</nav>
