769be87e19cdf622fb3391881c7712b8227eaafd
Caspar Leung <casparleung0219@gmail.com>
2022-03-30 00:51:47 +0100

    Update 404.php

---
+++ projects/small-business/jenkins-butchers-re/404.php
@@ -0,0 +1,26 @@
+<?php $this_page ="404Error"; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">	<!-- support most characters and symbols -->
+	<link rel="icon" href="imgs/site-icon.ico" type="image/x-icon">	<!-- site icon -->
+	<title><? echo $this_page; ?> | Jenkin's Butchers</title>	<!-- webpage title -->
+	<link href="scirpts/style.css" rel="stylesheet">	<!-- external style sheet -->
+	<meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Required for mobile devices -->
+	<!-- use of external google font -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Forum&family=IM+Fell+English+SC&family=Jost:wght@300&display=swap" rel="stylesheet"> 
+</head>
+<body>
+	<!-- || main content section || -->
+	<main>
+		<h1><? echo $this_page; ?></h1>	<!-- header | current Page -->
+		<p class="sub">Oh dear, We are really sorry, seems like something has gone wrong.</p>
+	</main>
+	<!-- || footer section || -->
+	<!-- section included with php -->
+	<? include $_SERVER['DOCUMENT_ROOT'] . '/projects/small-business/jenkins-butchers-re/php-include/footer.inc.php'; ?>
+
+</body>
+</html>
