20c1cdceb4e3ea025fff3ffba7b36d9a43afd9de
Caspar Leung <casparleung0219@gmail.com>
2022-04-30 19:07:26 +0100

    basic hero img and navigation

---
+++ projects/film-promotion/php-include/hero.inc.php
@@ -0,0 +1,10 @@
+<!-- hero image -->
+<div id="hero-mobile"></div>
+<div id="hero">
+    <div class="layer-bg layer" data-type="parallax" data-depth="0.10"></div>
+    <div class="layer-1 layer" data-type="parallax" data-depth="0.20"></div>
+    <div class="layer-2 layer" data-type="parallax" data-depth="0.50"></div>
+    <div class="layer-3 layer" data-type="parallax" data-depth="0.80"></div>
+    <div class="layer-overlay layer" data-type="parallax" data-depth="0.85"></div>
+    <div class="layer-4 layer" data-type="parallax" data-depth="1.00"></div>
+</div>
